0f7dde7776a5433473d71ea919a61b5c1113629a
Cauã Franco <caua.augusto3210@gmail.com>
2025-08-07 22:31:35 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,8 +1,8 @@
-
 #include<stdio.h>
 #include<stdlib.h>
 
 #define TAB_SIZE 10 // 10x10
+#define HAB_SIZE 5 // 5x5
 
 typedef struct
 {
@@ -82,30 +82,146 @@ void PosicionarNavio(NAVIO *navio, int tabuleiro[TAB_SIZE][TAB_SIZE])
 	navio->ativo = 1;
 }
 
+/* 
+	para verificar se da pra inserir a habilidade no tabuleiro vamos usar como base
+	o centro de cada habilidade: 
+						cone centro = [1][2], 
+						cruz centro = [2][2],
+						octaedro centro = [2][2]
+*/
+int ValidarPosicaoDaHabilidade(int habilidade[HAB_SIZE][HAB_SIZE], int x, int y)
+{
+	for(int i = 0; i < HAB_SIZE; i++)
+	{
+		for(int j = 0; j < HAB_SIZE; j++)
+		{
+			if(habilidade[i][j] == 0) continue;
+
+			int temp_x = x + (i - 2);
+			int temp_y = y + (j - 2);
+
+			if(temp_x < 0 || temp_x >= TAB_SIZE || temp_y < 0 || temp_y >= TAB_SIZE)
+			{
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+void DesenharHabilidade(int tabuleiro[TAB_SIZE][TAB_SIZE], int habilidade[HAB_SIZE][HAB_SIZE], int x, int y)
+{
+	for(int i = 0; i < HAB_SIZE; i++)
+	{
+		for(int j = 0; j < HAB_SIZE; j++)
+		{
+			if(habilidade[i][j] != 0)
+			{
+				int temp_x = x + (i - 2);
+				int temp_y = y + (j - 2);
+
+				if(temp_x >= 0 && temp_x < TAB_SIZE && temp_y >= 0 && temp_y < TAB_SIZE)
+				{
+					tabuleiro[temp_x][temp_y] = habilidade[i][j];
+				}
+			}
+		}
+	}
+}
+
+/*
+	0 0 5 0 0
+	0 5 5 5 0
+	5 5 5 5 5 
+	0 0 0 0 0
+	0 0 0 0 0
+*/
+void CriarCone(int cone[HAB_SIZE][HAB_SIZE])
+{	
+	for(int i = 0; i < HAB_SIZE; i++)
+	{
+		for(int j = 0; j < HAB_SIZE; j++)
+		{
+			cone[i][j] = 0;
+			if(i <= 2 && abs(j - 2) <= i){cone[i][j] = 5;}
+		}
+	}
+}
+
+/*
+	CRUZ
+	0 0 0 0 0
+	0 0 5 0 0
+	5 5 5 5 5 
+	0 0 5 0 0
+	0 0 0 0 0
+*/
+void CriarCruz(int cruz[HAB_SIZE][HAB_SIZE])
+{
+	for(int i = 0; i < HAB_SIZE; i++)
+	{
+		for(int j = 0; j < HAB_SIZE; j++)
+		{
+			cruz[i][j] = 0;
+			if(i == 0 && j == 2 || i == 4 && j == 2){continue;}
+			if(i == 2 || j == 2){cruz[i][j] = 5;}
+		}
+	}
+}
+
+/*
+	OCTAEDRO
+	0 0 0 0 0
+	0 0 5 0 0
+	0 5 5 5 0 
+	0 0 5 0 0
+	0 0 0 0 0
+*/
+void CriarOctaedro(int octaedro[HAB_SIZE][HAB_SIZE])
+{
+	for(int i = 0; i < HAB_SIZE; i++)
+	{
+		for(int j = 0; j < HAB_SIZE; j++)
+		{
+			if(i == 1 && j == 2){octaedro[i][j] = 5;}
+			else if(i == 2 && j >= 1 && j <= 3){octaedro[i][j] = 5;}
+			else if(i == 3 && j == 2){octaedro[i][j] = 5;}
+			else{octaedro[i][j] = 0;}
+		}
+	}
+}
+
 int main()
 {
 	/*
-		  0 1 2 3 4 5 6 7 8 9
-		  ___________________
-		0|0 0 0 0 0 0 0 0 0 0
-		1|0 0 0 0 0 0 0 0 0 0
-		2|0 0 3 0 0 0 0 0 0 0
-		3|0 0 3 0 0 0 0 0 0 0
-		4|0 0 3 0 0 0 0 0 0 0
-		5|0 0 0 0 0 0 0 0 0 0
-		6|0 0 0 0 0 0 0 3 3 3
-		7|0 0 0 0 0 0 0 0 0 0
-		8|0 0 0 0 0 0 0 0 0 0
-		9|0 0 0 0 0 0 0 0 0 0
+	  0 1 2 3 4 5 6 7 8 9
+	  -------------------
+	0|0 0 0 0 5 0 0 0 0 3
+	1|0 0 0 5 5 5 0 0 3 0
+	2|0 0 5 5 5 5 5 3 0 0
+	3|0 0 3 0 0 0 0 0 0 0
+	4|0 0 3 0 0 0 0 0 0 0
+	5|0 3 0 0 0 0 0 5 0 0
+	6|0 0 3 5 0 5 5 5 5 5
+	7|0 0 5 5 5 0 0 5 0 0
+	8|0 0 0 5 0 0 0 0 0 0
+	9|0 0 0 0 0 0 0 0 0 0
 	*/
 	int tabuleiro[TAB_SIZE][TAB_SIZE];
 	NAVIO n1;
 	NAVIO n2;
-
+	NAVIO n3;
+	NAVIO n4;
+	// HABILIDADES
+	int cone[HAB_SIZE][HAB_SIZE];
+	int cruz[HAB_SIZE][HAB_SIZE];
+	int octaedro[HAB_SIZE][HAB_SIZE];
+	
+	
 	// inicia o tabuleiro com 0
 	IniciarTabuleiro(tabuleiro);
 	
-	// ** navio 1 vetores: peças 1, 2 e 3
+	// **VERTICAL -> navio 1 vetores: peças 1, 2 e 3
 	n1.pecas.vetor1X = 2;
 	n1.pecas.vetor1Y = 2;
 	
@@ -115,12 +231,9 @@ int main()
 	n1.pecas.vetor3X = 4;
 	n1.pecas.vetor3Y = 2;
 	
-	if(ValidarPosicaoDoNavio(&n1, tabuleiro))
-	{
-		PosicionarNavio(&n1, tabuleiro);
-	}
+	if(ValidarPosicaoDoNavio(&n1, tabuleiro)){PosicionarNavio(&n1, tabuleiro);}
 	
-	// ** navio 2 vetores: peças 1, 2 e 3
+	// **HORIZONTAL -> navio 2 vetores: peças 1, 2 e 3
 	n2.pecas.vetor1X = 6;
 	n2.pecas.vetor1Y = 9;
 	
@@ -130,10 +243,54 @@ int main()
 	n2.pecas.vetor3X = 6;
 	n2.pecas.vetor3Y = 7;
 	
-	if(ValidarPosicaoDoNavio(&n2, tabuleiro))
+	if(ValidarPosicaoDoNavio(&n2, tabuleiro)){PosicionarNavio(&n2, tabuleiro);}
+	
+	// **DIAGONAL -> navio 3 vetores: peças 1, 2 e 3
+	n3.pecas.vetor1X = 0;
+	n3.pecas.vetor1Y = 9;
+	
+	n3.pecas.vetor2X = 1;
+	n3.pecas.vetor2Y = 8;
+	
+	n3.pecas.vetor3X = 2;
+	n3.pecas.vetor3Y = 7;
+
+	if(ValidarPosicaoDoNavio(&n3, tabuleiro)){PosicionarNavio(&n3, tabuleiro);}
+	
+	// **DIAGONAL -> navio 4 vetores: peças 1, 2 e 3
+	n4.pecas.vetor1X = 5;
+	n4.pecas.vetor1Y = 1;
+	
+	n4.pecas.vetor2X = 6;
+	n4.pecas.vetor2Y = 2;
+	
+	n4.pecas.vetor3X = 7;
+	n4.pecas.vetor3Y = 3;
+
+	if(ValidarPosicaoDoNavio(&n4, tabuleiro)){PosicionarNavio(&n4, tabuleiro);}
+	
+	// inicializando e desenhando habilidades no tabuleiro
+	// Cone
+	CriarCone(cone);
+	if(ValidarPosicaoDaHabilidade(cone, 2, 4))
 	{
-		PosicionarNavio(&n2, tabuleiro);
+		DesenharHabilidade(tabuleiro, cone, 2, 4);
 	}
+	
+	// Cruz
+	CriarCruz(cruz);
+	if(ValidarPosicaoDaHabilidade(cruz, 6, 7))
+	{
+		DesenharHabilidade(tabuleiro, cruz, 6, 7);
+	}
+	
+	// Octaedro
+	CriarOctaedro(octaedro);
+	if(ValidarPosicaoDaHabilidade(octaedro, 7, 3))
+	{
+		DesenharHabilidade(tabuleiro, octaedro, 7, 3);
+	}
+	
 	
 	// exibindo tabuleiro e finalizando o programa
 	ExibirTabuleiro(tabuleiro);
